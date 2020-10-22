0afaf1a6ddf46f229dc538207eecaafac46121f6
Kondwani Hara <kondwa@gmail.com>
2020-10-22 15:44:49 +0200

    tried a few display options and settled for something simple.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!--img alt="Vue logo" src="./assets/logo.png"-->
   <Resume msg="Kondwani Hara's Resume"/>
 </template>
 
@@ -22,5 +22,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
